21a07ce1e330a29ff7ab61583d2b250c369ac6da
5000user5000 <ck10600760@gmail.com>
2024-05-12 10:35:00 +0800

    debug : x 軸改正成 data size

--- plot_data.py
+++ plot_data.py
@@ -1,17 +1,19 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
 
 def plot_data(file_name, title):
     # 讀取數據
     data = pd.read_csv(file_name, header=None, names=['tx', 'rmb', 'wmb'])
-
+    x_ticks = np.linspace(5, 85, len(data)) # 設定是取 data size 5~85的數據
     # 為tx, rmb, wmb 繪圖
     for column in data.columns:
         plt.figure()
-        plt.plot(data[column])
+        plt.plot(x_ticks, data[column])
         plt.title(f"{title}: {column}")
-        plt.xlabel('Index')
+        plt.xlabel('data size')
         plt.ylabel(column)
+        
         plt.savefig(f"{title}_{column}.png")  # 儲存圖片
         plt.close()
 
